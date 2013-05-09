fb4035a675b387289b6c7ac6100329bac45f71c4
André Gaul <gaul@web-yard.de>
2013-05-09 19:20:58 +0200

    assert type==node in update handler

--- updates/node.js
+++ updates/node.js
@@ -1,5 +1,8 @@
 function (doc, req) {
   var newdoc = JSON.parse(req.body);
+  if (newdoc.type != 'node') {
+    return [null, 'Error: type != node'];
+  }
   if (doc) {
     newdoc._id = doc._id;
     newdoc._rev = doc._rev;
