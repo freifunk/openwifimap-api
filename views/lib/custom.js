959fa94fc2cfd32853498a1b72d8e4899095edd6
André Gaul <gaul@web-yard.de>
2013-05-26 18:08:42 +0200

    add mtime and ctime

--- views/lib/custom.js
+++ views/lib/custom.js
@@ -2,6 +2,8 @@ exports.customdata = function (doc) {
   return {
     hostname: doc.hostname,
     antennas: doc.antennas,
+    ctime: doc.ctime,
+    mtime: doc.mtime
     // add any data you like here!
   }
 }
