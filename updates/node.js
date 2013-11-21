7abfc4718edd787d6173b55844e10254028b01bf
Nico von Geyso <Nico.Geyso@FU-Berlin.de>
2013-11-21 18:55:09 +0100

    Fixed: prevent invalid node ids (with prefix '_')

--- updates/node.js
+++ updates/node.js
@@ -3,12 +3,22 @@ function (doc, req) {
   if (newdoc.type != 'node') {
     return [null, 'Error: type != node'];
   }
+
   if (doc) {
     newdoc._id = doc._id;
     newdoc._rev = doc._rev;
   }
+
+  // couchdb does not allow the prefix '_' as id for nodes
   if (!newdoc._id && req.id) {
-    newdoc._id = req.id;
+    newdoc._id = req.id.replace(/^_+/, '');
+  }
+
+  if ('links' in newdoc) {
+    for (var i = 0; i < newdoc.links.length; ++i) {
+        link = newdoc.links[i];
+        link.id = link.id.replace(/^_+/, '');
+    }
   }
 
   var date = (new Date()).toISOString();
